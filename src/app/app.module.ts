98a632b12f39785316f01be2a2151d690fbe5940
ZinabebaidZ123 <zinabebaid22@gmail.com>
2023-10-11 13:27:35 +0300

    'landing-page/products-page/products-details'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LandingPageModule } from './landing-page/landing-page.module';
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarPagesModule } from './navbar-pages/navbar-pages.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    LandingPageModule ,
+     SharedModule ,
+     NavbarPagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
